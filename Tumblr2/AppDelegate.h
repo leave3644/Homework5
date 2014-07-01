ba488946014383b4ea1dd8671dfd48cc4647daaa
Daniel O'Rorke <danielo@storypaydig-lm.corp.yahoo.com>
2014-06-30 20:01:06 -0700

    Initial Commit

---
+++ Tumblr2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tumblr2
+//
+//  Created by Daniel O'Rorke on 6/30/14.
+//  Copyright (c) 2014 Daniel O'Rorke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tumblr2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tumblr2
+//
+//  Created by Daniel O'Rorke on 6/30/14.
+//  Copyright (c) 2014 Daniel O'Rorke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
